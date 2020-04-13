38cb7a105040d809fa6d009d12e1d7ba64650f69
JackMerryYoung <52907970+JackMerryYoung@users.noreply.github.com>
2020-04-13 14:17:36 +0800

    Create function.h

---
+++ 1.1/function.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+using namespace std;
+extern HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+void draw_start()
+{
+	SetConsoleTextAttribute(handle,FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+	cout << "      ■   ■■     ■■■" << endl;
+	cout << "      ■ ■    ■ ■      " << endl;
+	cout << "      ■ ■    ■   ■■  " << endl;
+	cout << "■    ■ ■    ■       ■" << endl;
+	cout << "  ■■     ■■   ■■■  " << endl;
+	SetConsoleTextAttribute(handle,FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+}
+void draw_menu()
+{
+	cout << "------------------------------------" << endl;
+	cout << "|           JOS main menu          |" << endl;
+	cout << "------------------------------------" << endl;
+	cout << "|             1.shutdown           |" << endl;
+	cout << "------------------------------------" << endl;
+	cout << "|           2.EScript(REPL)        |" << endl;
+	cout << "------------------------------------" << endl;
+}
+bool closeJOS()
+{
+	int yn = MessageBox(NULL,TEXT("Shutdown?"),TEXT("Prompt"),MB_YESNO);
+	if(yn == IDYES) {
+		for(int i = 0; i < 3; i ++) {
+			cout << "shutdown";
+			for(int i = 0; i < 3; i ++) {
+				cout << ".";
+				Sleep(1000);
+			}
+			system("cls");
+		}
+		MessageBox(NULL,TEXT("Successfully!"),TEXT("Prompt"),MB_OK);
+		return true;
+	} else {
+		MessageBox(NULL,TEXT("Canceled"),TEXT("Prompt"),MB_OK);
+		return false;
+	}
+}
+void EScript() {
+	system("start EScript 1 0");
+}
