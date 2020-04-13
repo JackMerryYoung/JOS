e5b00fa5046fd12015a8ff6ba2100d53fffb11b8
JackMerryYoung <52907970+JackMerryYoung@users.noreply.github.com>
2020-04-13 14:18:38 +0800

    JOS Version 1.1

---
+++ 1.1/JOS.cpp
@@ -0,0 +1,25 @@
+#include "function.h"
+using namespace std;
+int main() {
+	draw_start();
+	cout << "Welcome to JOS!";
+	getch();
+	system("cls");
+	while(true) {
+		while(true) {
+			draw_menu();
+			int opr = getch();
+			if(opr == '1') {
+				system("cls");
+				bool close_open = closeJOS();
+				if(close_open == true) return 0;
+			}
+			if(opr == '2') {
+				system("cls");
+				EScript(); 
+			}
+		}
+		system("cls");
+	}
+	return 0;
+}
